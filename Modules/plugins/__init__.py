f7cebea41b47bc328a2fdf4c0266f1d5425aa70f
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-26 03:24:44 +0530

    Create __init__.py

---
+++ Modules/plugins/__init__.py
@@ -0,0 +1,6 @@
+# plugins/__init__.py
+
+# This file marks the plugins folder as a Python package.
+# You can optionally initialize variables or import common functions here.
+
+__all__ = ["bot", "sudo"]
