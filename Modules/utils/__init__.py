c5668f25244e1a7692a3bad2ee9e33a0541f037a
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-26 03:47:10 +0530

    Create __init__.py

---
+++ Modules/utils/__init__.py
@@ -0,0 +1,5 @@
+# utils/__init__.py
+
+# This file is intentionally left blank to treat 'utils' as a package.
+# Modules like database.py, helpers.py, decorators.py, and inline.py
+# can now be imported from this package.
