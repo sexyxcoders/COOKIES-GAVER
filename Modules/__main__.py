b7ee61e500da9df28b342a922dc69b7d6c7a2e66
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-26 03:16:13 +0530

    Update and rename _main_.py to __main__.py

--- Modules/__main__.py
+++ Modules/__main__.py
@@ -1,8 +1,8 @@
 import asyncio
 import importlib
 import os
-
-from cookies import app
+from pyrogram import idle
+from __init__ import app
 
 async def init():
     # Dynamically import all plugins in 'plugins' folder
@@ -12,7 +12,7 @@ async def init():
             if file.endswith(".py") and file != "__init__.py":
                 module_path = os.path.relpath(os.path.join(root, file), start=os.path.dirname(__file__))
                 module_name = module_path.replace(os.path.sep, ".").replace(".py", "")
-                importlib.import_module(f"cookies.{module_name}")
+                importlib.import_module(module_name)
 
     print("Bot plugins loaded.")
     await app.start()
@@ -20,5 +20,4 @@ async def init():
     await idle()
 
 if __name__ == "__main__":
-    from pyrogram import idle
     asyncio.run(init())
