0347649ea4222ac68c1f5c8ca78fd5002da08ead
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-26 10:10:57 +0530

    Update config.py

--- Modules/config.py
+++ Modules/config.py
@@ -14,8 +14,8 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 BANNED_USERS = filters.user()
 START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/i5ngz3.jpg")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/your_support_chat")
-MUST_JOIN = getenv("MUST_JOIN", "https://t.me/YourChannel")  # Required channel/group
-LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1000000000"))
+MUST_JOIN = getenv("MUST_JOIN", "https://t.me/TNCnetwork")  # Required channel/group
+LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1003065367480"))
 
 # ===== Cookies =====
 COOKIES_FILE = os.path.join(os.path.dirname(__file__), "cookies", "default_cookies.txt")
